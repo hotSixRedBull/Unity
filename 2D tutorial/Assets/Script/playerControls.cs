8140176a8033ec6197dfbc820532bd5783aaed61
software.lee <akkalee1207@gmail.com>
2018-12-02 23:34:35 +0900

    [Alan] 2DTutorial end

--- 2D tutorial/Assets/Script/playerControls.cs
+++ 2D tutorial/Assets/Script/playerControls.cs
@@ -3,23 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playerControls : MonoBehaviour {
+	Animator anim;
 	public Rigidbody2D rb;
 	public Transform groundCheck;
 	public float groundCheckRadius;
 	public LayerMask whatIsGround;
 	private bool onGround;
+	float jumpForce = 700f;
 
 	// Use this for initialization
 	void Start () {
 			rb = GetComponent<Rigidbody2D>();	
+			anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		rb.velocity = new Vector2(3,  rb.velocity.y);
+		//rb.velocity = new Vector2(3,  rb.velocity.y);
 		onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
 		if (Input.GetMouseButtonDown(0) && onGround) {
-			rb.velocity = new Vector2( rb.velocity.x, 3);
+			rb.AddForce (Vector2.up * jumpForce);
 		}
 	}
+
+
+
 }
